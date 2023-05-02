5d0d53a50cf842327a1496dbe67d70dbae1037e2
Pravar Anu <pravaranu6@gmail.com>
2023-05-02 20:55:27 +0530

    Created a category array and render it using app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
+const categories =[
+  {
+    id: 1, 
+    title: "Hats" 
+  },
+  {
+    id: 2,
+    title: "Jackets"
+  },
+  {
+    id: 3,
+    title: "Sneakers"
+  },
+  {
+    id: 4,
+    title: "Womens"
+  },
+  {
+    id: 5,
+    title: "Mens"
+  }
+]
+
 function App() {
   return (
     <div className="App">
-      Hello
+      {
+        categories.map(({title})=>{
+          return (
+            <div className='category-container'>
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
