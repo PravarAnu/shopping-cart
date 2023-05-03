035c647027fa2630b45941099d1f02b8a6ecf72c
Pravar Anu <pravaranu6@gmail.com>
2023-05-03 13:21:49 +0530

    Made cart Item list and cart dropdown functional

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,31 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../context/cart.context";
+import CartItem from "../cart-item/cart-item.component";
+
 import Button from "../button/button.component";
+
 
 import "./cart-dropdown.style.scss"
 
 
 const CartDropdown = () => {
-    <div className='cart-dropdown-container'>
-        <div className='cart-items' />
+    const { cartItems } = useContext(CartContext);
+
+    return (
+      <div className='cart-dropdown-container'>
+        <div className='cart-items'>
+          {cartItems.length ? (
+            cartItems.map((cartItem) => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))
+          ) : (
+            <span className='empty-message'>Your cart is empty</span>
+          )}
+        </div>
         <Button>GO TO CHECKOUT</Button>
-    </div>
+      </div>
+    );
 }
 
 export default CartDropdown;
